e066d5b25f4779dda370b9dae5f8b8a2efad6b68
Robert <jrobertgardzinski@gmail.com>
2019-06-14 00:09:07 +0200

    Redesign Field class to use builder pattern

--- src/main/java/gamemechanics/Field.java
+++ src/main/java/gamemechanics/Field.java
@@ -3,35 +3,69 @@ package gamemechanics;
 import java.util.Map;
 
 import gamemechanics.enums.PolishAlphabet;
+import gamemechanics.enums.Premium;
 
 public class Field {
-	Coordinates coordinates;
-	Tile tile;
+	private Coordinates coordinates;
+	private Tile tile;
+	private Premium premium;
 
-	public Field(int x, int y, char letter) {
-		super();
-		this.coordinates = new Coordinates(x, y);
-		
-		this.tile = new Tile(PolishAlphabet.A);
+	public Field(Builder builder) {
+		this.coordinates = builder.coordinates;
+		this.tile = builder.tile;
+		this.premium = builder.premium;
 	}
-	
-	public Field(Coordinates coordinates, Tile tile) {
-		super();
-		this.coordinates = coordinates;
-		this.tile = tile;
-	}
+
 	public Coordinates getCoordinates() {
 		return coordinates;
 	}
+
 	public void setCoordinates(Coordinates coordinates) {
 		this.coordinates = coordinates;
 	}
+
 	public Tile getTile() {
 		return tile;
 	}
+
 	public void setTile(Tile tile) {
 		this.tile = tile;
 	}
-	
-	
+
+	public Premium getPremium() {
+		return premium;
+	}
+
+	public static class Builder {
+		private Coordinates coordinates;
+		private Tile tile;
+		private Premium premium;
+
+		public Builder(Coordinates coordinates) {
+			this.coordinates = coordinates;
+		}
+
+		public Builder(int x, int y) {
+			this.coordinates = new Coordinates(x, y);
+		}
+
+		public Builder setTile(Tile tile) {
+			this.tile = tile;
+			return this;
+		}
+		
+		public Builder setTile(char letter) {
+			this.tile = new Tile(letter);
+			return this;
+		}
+
+		public Builder setPremium(Premium premium) {
+			this.premium = premium;
+			return this;
+		}
+
+		public Field build() {
+			return new Field(this);
+		}
+	}
 }
--- src/test/java/gamemechanics/GameBoardTest.java
+++ src/test/java/gamemechanics/GameBoardTest.java
@@ -28,11 +28,11 @@ class GameBoardTest {
 		@Test
 		void shouldNotAcceptPlacingFirstWordNotOnTheCenterOfTheGameBoard() {
 			List<Field> playedTiles = new LinkedList<>();
-			playedTiles.add(new Field(1,8,'A'));
-			playedTiles.add(new Field(1,9,'W'));
-			playedTiles.add(new Field(1,10,'I'));
-			playedTiles.add(new Field(1,11,'Z'));
-			playedTiles.add(new Field(1,12,'O'));
+			playedTiles.add(new Field.Builder(1,8).setTile('A').build());
+			playedTiles.add(new Field.Builder(1,9).setTile('W').build());
+			playedTiles.add(new Field.Builder(1,10).setTile('I').build());
+			playedTiles.add(new Field.Builder(1,11).setTile('Z').build());
+			playedTiles.add(new Field.Builder(1,12).setTile('O').build());
 			
 			assertThrows(IllegalArgumentException.class, () -> { gameBoard.playTiles(playedTiles); });
 		}
@@ -40,11 +40,11 @@ class GameBoardTest {
 		@Test
 		void centerFieldShouldHasDoubleWordPremium() {
 			List<Field> playedTiles = new LinkedList<>();
-			playedTiles.add(new Field(8,8,'A'));
-			playedTiles.add(new Field(8,9,'W'));
-			playedTiles.add(new Field(8,10,'I'));
-			playedTiles.add(new Field(8,11,'Z'));
-			playedTiles.add(new Field(8,12,'O'));
+			playedTiles.add(new Field.Builder(8,8).setTile('A').build());
+			playedTiles.add(new Field.Builder(8,9).setTile('W').build());
+			playedTiles.add(new Field.Builder(8,10).setTile('I').build());
+			playedTiles.add(new Field.Builder(8,11).setTile('Z').build());
+			playedTiles.add(new Field.Builder(8,12).setTile('O').build());
 			
 			assertTrue(gameBoard.getPremiumIfExists(new Coordinates(8, 8)).equals(Premium.DOUBLE_WORD));
 		}
@@ -52,7 +52,7 @@ class GameBoardTest {
 		@Test
 		void firstPlayedWordConsistAtLeastOfTwoTiles() {
 			List<Field> playedTiles = new LinkedList<Field>();
-			playedTiles.add(new Field(8,8,'A'));
+			playedTiles.add(new Field.Builder(8,8).setTile('A').build());
 			
 			assertThrows(IllegalArgumentException.class, () -> { gameBoard.playTiles(playedTiles); });
 		}
@@ -72,52 +72,52 @@ class GameBoardTest {
 			// AWIZO
 			// ZDRÓW
 			// BÓL
-			tilesOnBoard.add(new Field(8,8,'A'));
-			tilesOnBoard.add(new Field(8,9,'W'));
-			tilesOnBoard.add(new Field(8,10,'I'));
-			tilesOnBoard.add(new Field(8,11,'Z'));
-			tilesOnBoard.add(new Field(8,12,'O'));
-			tilesOnBoard.add(new Field(9,11,'D'));
-			tilesOnBoard.add(new Field(10,11,'R'));
-			tilesOnBoard.add(new Field(11,11,'Ó'));
-			tilesOnBoard.add(new Field(12,11,'W'));
-			tilesOnBoard.add(new Field(11,10,'B'));
-			tilesOnBoard.add(new Field(11,12,'L'));
+			tilesOnBoard.add(new Field.Builder(8,8).setTile('A').build());
+			tilesOnBoard.add(new Field.Builder(8,9).setTile('W').build());
+			tilesOnBoard.add(new Field.Builder(8,10).setTile('I').build());
+			tilesOnBoard.add(new Field.Builder(8,11).setTile('Z').build());
+			tilesOnBoard.add(new Field.Builder(8,12).setTile('O').build());
+			tilesOnBoard.add(new Field.Builder(9,11).setTile('D').build());
+			tilesOnBoard.add(new Field.Builder(10,11).setTile('R').build());
+			tilesOnBoard.add(new Field.Builder(11,11).setTile('Ó').build());
+			tilesOnBoard.add(new Field.Builder(12,11).setTile('W').build());
+			tilesOnBoard.add(new Field.Builder(11,10).setTile('B').build());
+			tilesOnBoard.add(new Field.Builder(11,12).setTile('L').build());
 			
 			gameBoard.setTilesArrangement(tilesOnBoard);
 		}
 		
 		@Test
 		void playedTilesShouldBePlacedInOneLine() {
-			tilesToPlay.add(new Field(6,11,'P'));
-			tilesToPlay.add(new Field(7,11,'O'));
-			tilesToPlay.add(new Field(7,12,'D'));
+			tilesToPlay.add(new Field.Builder(6,11).setTile('P').build());
+			tilesToPlay.add(new Field.Builder(7,11).setTile('O').build());
+			tilesToPlay.add(new Field.Builder(7,12).setTile('D').build());
 			
 			assertThrows(IllegalArgumentException.class, () -> { gameBoard.playTiles(tilesToPlay); });
 		}
 
 		@Test
 		void playedTilesShouldNotBeSeparated() {
-			tilesToPlay.add(new Field(7,5,'T'));
+			tilesToPlay.add(new Field.Builder(7,5).setTile('T').build());
 			// take note about space between T and other tiles
-			tilesToPlay.add(new Field(7,7,'A'));
-			tilesToPlay.add(new Field(7,8,'M'));
+			tilesToPlay.add(new Field.Builder(7,7).setTile('A').build());
+			tilesToPlay.add(new Field.Builder(7,8).setTile('M').build());
 			
 			assertThrows(IllegalArgumentException.class, () -> { gameBoard.playTiles(tilesToPlay); });			
 		}
 
 		@Test
 		void atLeastOneTileShouldAdhereToOneAlreadyPlacedOnGameBoard() {
-			tilesToPlay.add(new Field(7,6,'J'));
-			tilesToPlay.add(new Field(8,6,'A'));
+			tilesToPlay.add(new Field.Builder(7,6).setTile('J').build());
+			tilesToPlay.add(new Field.Builder(8,6).setTile('A').build());
 			
 			assertThrows(IllegalArgumentException.class, () -> { gameBoard.playTiles(tilesToPlay); });
 		}
 		
 		@Test
 		void cannotPlaceTileOnOccupiedField() {
-			tilesToPlay.add(new Field(8,8,'T'));
-			tilesToPlay.add(new Field(8,9,'Y'));
+			tilesToPlay.add(new Field.Builder(8,8).setTile('T').build());
+			tilesToPlay.add(new Field.Builder(8,9).setTile('Y').build());
 			
 			assertThrows(IllegalArgumentException.class, () -> { gameBoard.playTiles(tilesToPlay); });
 		}
